7023f132660e948419c5c95a67c88d0bb8dc8132
iqbal45778 <163950305+iqbal45778@users.noreply.github.com>
2024-11-20 06:17:38 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=STRIX kernel by fiqri19102002 @ github
+kernel.string=PERF kernel by predator @ github
 do.devicecheck=1
 do.modules=0
 do.systemless=1
